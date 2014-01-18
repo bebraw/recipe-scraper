6e9e199916cecc114be8df9f93cea2a7d001d87f
Juho Vepsalainen <bebraw@gmail.com>
2014-01-18 20:28:05 +0100

    Scrape recipe name

---
+++ tests/scrape.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var path = require('path');
+var read = require('../lib/file').read;
+var scrape = require('../lib/scrape');
+
+
+module.exports = function() {
+    read(path.join(__dirname, './data/recipe.html'), function(err, d) {
+        if(err) {
+            return console.error(err);
+        }
+
+        scrape(d, function(err, d) {
+            if(err) {
+                return console.error(err);
+            }
+
+            assert.deepEqual(d, {
+                name: '\"Pasta\" Primavera'
+            });
+        });
+    });
+};
+
